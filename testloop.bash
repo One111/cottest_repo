7bc7493051d55d2885313422453060c03acf5eaf
One111 <8937781+One111@users.noreply.github.com>
2022-03-22 20:08:18 +0300

    Create testloop.bash

---
+++ testloop.bash
@@ -0,0 +1,9 @@
+# from https://linuxhint.com/30_bash_loop_examples/#l1
+
+#!/bin/bash
+# for-in loop to read a list of numbers
+for n in 10 11 17 25
+do
+  # Print each number
+  echo "The number is $n"
+done

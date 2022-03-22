d52b749d81502cb0fd7ceb762fca311e1eeb2763
One111 <8937781+One111@users.noreply.github.com>
2022-03-22 20:09:39 +0300

    Add numbers and rename print statement

--- testloop.bash
+++ testloop.bash
@@ -2,8 +2,8 @@
 
 #!/bin/bash
 # for-in loop to read a list of numbers
-for n in 10 11 17 25
+for n in 10 11 17 25 41 11 11 5
 do
   # Print each number
-  echo "The number is $n"
+  echo "Your mama is $n"
 done
